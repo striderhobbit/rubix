0b0158c40317850cf9902d19f2a49a3072f8a559
striderhobbit <mariuskoehler@yahoo.de>
2024-02-14 14:18:46 +0100

    MAJOR permutations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, HostBinding } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, HostBinding, HostListener } from '@angular/core';
+import { at, intersection, range } from 'lodash';
+import { Subject, concatMap, take, tap, toArray } from 'rxjs';
 import { Quaternion, Vector3 } from 'three';
 
-type Face = 'back' | 'down' | 'front' | 'left' | 'right' | 'up';
+type Face = 'B' | 'D' | 'F' | 'L' | 'R' | 'U';
+type Layer = 'B' | 'D' | 'E' | 'F' | 'L' | 'M' | 'R' | 'S' | 'U';
 
 interface Cubicle {
   index: number;
@@ -11,7 +13,54 @@ interface Cubicle {
     y: number;
     z: number;
   };
-  faces: Face[];
+  layers: Layer[];
+}
+
+class Permutation {
+  #map: number[];
+
+  constructor(map: number | number[]) {
+    if (typeof map === 'number') {
+      map = range(map);
+    }
+
+    this.#map = map.slice();
+  }
+
+  static fromDisjointCycles(cycles: string, n: number): Permutation {
+    return new Permutation(
+      Array.from(cycles.matchAll(/\((\d+(\s+\d+)*)\)/g))
+        .map(([_, cycle]) => cycle.split(/\s+/).map(Number))
+        .reduce((map, cycle) => {
+          cycle.forEach((x, i) => (map[x] = cycle[(i + 1) % cycle.length]));
+
+          return map;
+        }, range(n))
+    );
+  }
+
+  apply(permutation: Permutation): Permutation {
+    this.#map = at(permutation.#map, this.#map);
+
+    return this;
+  }
+
+  image(x: number): number {
+    return this.#map[x];
+  }
+
+  inverse(): Permutation {
+    return new Permutation(
+      this.#map.reduce(
+        (inverse, y, x) => Object.assign(inverse, { [y]: x }),
+        Array(this.#map.length)
+      )
+    );
+  }
+
+  preimage(y: number): number {
+    return this.#map.indexOf(y);
+  }
 }
 
 interface AxialRotation {
@@ -52,7 +101,7 @@ class Rotation {
     this.#axialRotation = this.#toAxialRotation();
   }
 
-  append(axialRotation: AxialRotation): void {
+  apply(axialRotation: AxialRotation): Rotation {
     this.#quaternion.premultiply(
       new Quaternion().setFromAxisAngle(
         axialRotation.axis.clone().normalize(),
@@ -61,6 +110,8 @@ class Rotation {
     );
 
     this.#axialRotation = this.#toAxialRotation();
+
+    return this;
   }
 
   #toAxialRotation(): AxialRotation {
@@ -85,97 +136,319 @@ class Rotation {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  @HostBinding('attr.data-move') move?: Face = 'back';
+  @HostBinding('attr.data-move') move?: Layer;
 
-  private animationEndSubject: Subject<{ target: Face }> = new Subject();
-  private free?: boolean;
-
-  protected faces: Face[] = Array(27)
-    .fill(['back', 'down', 'front', 'left', 'right', 'up'])
-    .flat();
-
-  protected cubicles: Cubicle[] = [
-    { index: 0, coords: { x: 0, y: 0, z: -1 }, faces: ['left', 'up', 'back'] },
-    { index: 6, coords: { x: 0, y: 0, z: 0 }, faces: ['left', 'up'] },
-    { index: 12, coords: { x: 0, y: 0, z: 1 }, faces: ['left', 'up', 'front'] },
-    { index: 18, coords: { x: 0, y: 1, z: -1 }, faces: ['left', 'back'] },
-    { index: 24, coords: { x: 0, y: 1, z: 0 }, faces: ['left'] },
-    { index: 30, coords: { x: 0, y: 1, z: 1 }, faces: ['left', 'front'] },
-    {
-      index: 36,
-      coords: { x: 0, y: 2, z: -1 },
-      faces: ['left', 'down', 'back'],
-    },
-    { index: 42, coords: { x: 0, y: 2, z: 0 }, faces: ['left', 'down'] },
-    {
-      index: 48,
-      coords: { x: 0, y: 2, z: 1 },
-      faces: ['left', 'down', 'front'],
-    },
-    { index: 54, coords: { x: 1, y: 0, z: -1 }, faces: ['up', 'back'] },
-    { index: 60, coords: { x: 1, y: 0, z: 0 }, faces: ['up'] },
-    { index: 66, coords: { x: 1, y: 0, z: 1 }, faces: ['up', 'front'] },
-    { index: 72, coords: { x: 1, y: 1, z: -1 }, faces: ['back'] },
-    { index: 78, coords: { x: 1, y: 1, z: 0 }, faces: [] },
-    { index: 84, coords: { x: 1, y: 1, z: 1 }, faces: ['front'] },
-    { index: 90, coords: { x: 1, y: 2, z: -1 }, faces: ['down', 'back'] },
-    { index: 96, coords: { x: 1, y: 2, z: 0 }, faces: ['down'] },
-    { index: 102, coords: { x: 1, y: 2, z: 1 }, faces: ['down', 'front'] },
-    {
-      index: 108,
-      coords: { x: 2, y: 0, z: -1 },
-      faces: ['right', 'up', 'back'],
-    },
-    { index: 114, coords: { x: 2, y: 0, z: 0 }, faces: ['right', 'up'] },
-    {
-      index: 120,
-      coords: { x: 2, y: 0, z: 1 },
-      faces: ['right', 'up', 'front'],
-    },
-    { index: 126, coords: { x: 2, y: 1, z: -1 }, faces: ['right', 'back'] },
-    { index: 132, coords: { x: 2, y: 1, z: 0 }, faces: ['right'] },
-    { index: 138, coords: { x: 2, y: 1, z: 1 }, faces: ['right', 'front'] },
-    {
-      index: 144,
-      coords: { x: 2, y: 2, z: -1 },
-      faces: ['right', 'down', 'back'],
-    },
-    { index: 150, coords: { x: 2, y: 2, z: 0 }, faces: ['right', 'down'] },
-    {
-      index: 156,
-      coords: { x: 2, y: 2, z: 1 },
-      faces: ['right', 'down', 'front'],
-    },
-  ];
-
-  protected rotation: Rotation = new Rotation();
-
-  protected onAnimationEnd(cubicle: Cubicle): void {
-    if (cubicle.faces.length === 1) {
-      this.animationEndSubject.next({ target: cubicle.faces[0] });
-    }
-  }
-
-  protected onMouseDown(event: MouseEvent): void {
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent): void {
     this.free = true;
   }
 
-  protected onMouseLeave(event: MouseEvent): void {
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event: MouseEvent): void {
     delete this.free;
   }
 
-  protected onMouseMove(event: MouseEvent): void {
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
     if (this.free) {
       const axis = new Vector3(-event.movementY, event.movementX, 0);
 
-      this.rotation.append({
+      this.rotation.apply({
         axis,
-        angle: axis.length() / 100,
+        angle: axis.length() / 80,
       });
     }
   }
 
-  protected onMouseUp(event: MouseEvent): void {
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent): void {
     delete this.free;
+  }
+
+  private free?: boolean;
+
+  private moves: Subject<Layer> = new Subject();
+
+  private permutations: Record<Layer, Permutation> = {
+    B: new Permutation([
+      36, 40, 38, 37, 41, 39, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 90,
+      94, 92, 91, 95, 93, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 144,
+      148, 146, 145, 149, 147, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+      18, 22, 20, 19, 23, 21, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      126, 130, 128, 127, 131, 129, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+      105, 106, 107, 0, 4, 2, 1, 5, 3, 114, 115, 116, 117, 118, 119, 120, 121,
+      122, 123, 124, 125, 54, 58, 56, 55, 59, 57, 132, 133, 134, 135, 136, 137,
+      138, 139, 140, 141, 142, 143, 108, 112, 110, 109, 113, 111, 150, 151, 152,
+      153, 154, 155, 156, 157, 158, 159, 160, 161,
+    ]),
+    D: new Permutation([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 51, 49, 52,
+      50, 48, 53, 105, 103, 106, 104, 102, 107, 159, 157, 160, 158, 156, 161,
+      54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      45, 43, 46, 44, 42, 47, 96, 97, 98, 99, 100, 101, 153, 151, 154, 152, 150,
+      155, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+      122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
+      137, 138, 139, 140, 141, 142, 143, 39, 37, 40, 38, 36, 41, 93, 91, 94, 92,
+      90, 95, 147, 145, 148, 146, 144, 149,
+    ]),
+    E: new Permutation([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 33, 31, 34,
+      32, 30, 35, 87, 85, 88, 86, 84, 89, 141, 139, 142, 140, 138, 143, 36, 37,
+      38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+      56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 27, 25,
+      28, 26, 24, 29, 78, 79, 80, 81, 82, 83, 135, 133, 136, 134, 132, 137, 90,
+      91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
+      107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+      122, 123, 124, 125, 21, 19, 22, 20, 18, 23, 75, 73, 76, 74, 72, 77, 129,
+      127, 130, 128, 126, 131, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153,
+      154, 155, 156, 157, 158, 159, 160, 161,
+    ]),
+    F: new Permutation([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 120, 123, 122, 125, 121, 124, 18,
+      19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 66, 69, 68, 71, 67, 70, 36,
+      37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 12, 15, 14, 17, 13, 16, 54,
+      55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 138, 141, 140, 143, 139, 142,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 30, 33, 32, 35, 31, 34,
+      108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 156, 159, 158,
+      161, 157, 160, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137,
+      102, 105, 104, 107, 103, 106, 144, 145, 146, 147, 148, 149, 150, 151, 152,
+      153, 154, 155, 48, 51, 50, 53, 49, 52,
+    ]),
+    L: new Permutation([
+      17, 12, 13, 15, 16, 14, 35, 30, 31, 33, 34, 32, 53, 48, 49, 51, 52, 50,
+      11, 6, 7, 9, 10, 8, 24, 25, 26, 27, 28, 29, 47, 42, 43, 45, 46, 44, 5, 0,
+      1, 3, 4, 2, 23, 18, 19, 21, 22, 20, 41, 36, 37, 39, 40, 38, 54, 55, 56,
+      57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+      75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
+      93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+      109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123,
+      124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138,
+      139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153,
+      154, 155, 156, 157, 158, 159, 160, 161,
+    ]),
+    M: new Permutation([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+      39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 71, 66, 67,
+      69, 70, 68, 89, 84, 85, 87, 88, 86, 107, 102, 103, 105, 106, 104, 65, 60,
+      61, 63, 64, 62, 78, 79, 80, 81, 82, 83, 101, 96, 97, 99, 100, 98, 59, 54,
+      55, 57, 58, 56, 77, 72, 73, 75, 76, 74, 95, 90, 91, 93, 94, 92, 108, 109,
+      110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124,
+      125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139,
+      140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154,
+      155, 156, 157, 158, 159, 160, 161,
+    ]),
+    R: new Permutation([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+      39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+      57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+      75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
+      93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 145,
+      146, 149, 147, 148, 144, 127, 128, 131, 129, 130, 126, 109, 110, 113, 111,
+      112, 108, 151, 152, 155, 153, 154, 150, 132, 133, 134, 135, 136, 137, 115,
+      116, 119, 117, 118, 114, 157, 158, 161, 159, 160, 156, 139, 140, 143, 141,
+      142, 138, 121, 122, 125, 123, 124, 120,
+    ]),
+    S: new Permutation([
+      0, 1, 2, 3, 4, 5, 114, 117, 116, 119, 115, 118, 12, 13, 14, 15, 16, 17,
+      18, 19, 20, 21, 22, 23, 60, 63, 62, 65, 61, 64, 30, 31, 32, 33, 34, 35,
+      36, 37, 38, 39, 40, 41, 6, 9, 8, 11, 7, 10, 48, 49, 50, 51, 52, 53, 54,
+      55, 56, 57, 58, 59, 132, 135, 134, 137, 133, 136, 66, 67, 68, 69, 70, 71,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      90, 91, 92, 93, 94, 95, 24, 27, 26, 29, 25, 28, 102, 103, 104, 105, 106,
+      107, 108, 109, 110, 111, 112, 113, 150, 153, 152, 155, 151, 154, 120, 121,
+      122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 96, 99, 98, 101, 97,
+      100, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 42, 45,
+      44, 47, 43, 46, 156, 157, 158, 159, 160, 161,
+    ]),
+    U: new Permutation([
+      112, 109, 111, 108, 110, 113, 58, 55, 57, 54, 56, 59, 4, 1, 3, 0, 2, 5,
+      18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+      36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+      118, 115, 117, 114, 116, 119, 60, 61, 62, 63, 64, 65, 10, 7, 9, 6, 8, 11,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
+      107, 124, 121, 123, 120, 122, 125, 70, 67, 69, 66, 68, 71, 16, 13, 15, 12,
+      14, 17, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138,
+      139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153,
+      154, 155, 156, 157, 158, 159, 160, 161,
+    ]),
+  };
+
+  protected animations: Subject<Layer[]> = new Subject();
+
+  protected cubicles: Cubicle[] = [
+    {
+      index: 0,
+      coords: { x: 0, y: 0, z: -1 },
+      layers: ['L', 'U', 'B'],
+    },
+    {
+      index: 6,
+      coords: { x: 0, y: 0, z: 0 },
+      layers: ['L', 'U', 'S'],
+    },
+    {
+      index: 12,
+      coords: { x: 0, y: 0, z: 1 },
+      layers: ['L', 'U', 'F'],
+    },
+    {
+      index: 18,
+      coords: { x: 0, y: 1, z: -1 },
+      layers: ['L', 'B', 'E'],
+    },
+    {
+      index: 24,
+      coords: { x: 0, y: 1, z: 0 },
+      layers: ['L', 'E', 'S'],
+    },
+    {
+      index: 30,
+      coords: { x: 0, y: 1, z: 1 },
+      layers: ['L', 'F', 'E'],
+    },
+    {
+      index: 36,
+      coords: { x: 0, y: 2, z: -1 },
+      layers: ['L', 'D', 'B'],
+    },
+    {
+      index: 42,
+      coords: { x: 0, y: 2, z: 0 },
+      layers: ['L', 'D', 'S'],
+    },
+    {
+      index: 48,
+      coords: { x: 0, y: 2, z: 1 },
+      layers: ['L', 'D', 'F'],
+    },
+    {
+      index: 54,
+      coords: { x: 1, y: 0, z: -1 },
+      layers: ['U', 'B', 'M'],
+    },
+    {
+      index: 60,
+      coords: { x: 1, y: 0, z: 0 },
+      layers: ['U', 'M', 'S'],
+    },
+    {
+      index: 66,
+      coords: { x: 1, y: 0, z: 1 },
+      layers: ['U', 'F', 'M'],
+    },
+    {
+      index: 72,
+      coords: { x: 1, y: 1, z: -1 },
+      layers: ['B', 'E', 'M'],
+    },
+    { index: 78, coords: { x: 1, y: 1, z: 0 }, layers: ['E', 'M', 'S'] },
+    {
+      index: 84,
+      coords: { x: 1, y: 1, z: 1 },
+      layers: ['F', 'E', 'M'],
+    },
+    {
+      index: 90,
+      coords: { x: 1, y: 2, z: -1 },
+      layers: ['D', 'B', 'M'],
+    },
+    {
+      index: 96,
+      coords: { x: 1, y: 2, z: 0 },
+      layers: ['D', 'M', 'S'],
+    },
+    {
+      index: 102,
+      coords: { x: 1, y: 2, z: 1 },
+      layers: ['D', 'F', 'M'],
+    },
+    {
+      index: 108,
+      coords: { x: 2, y: 0, z: -1 },
+      layers: ['R', 'U', 'B'],
+    },
+    {
+      index: 114,
+      coords: { x: 2, y: 0, z: 0 },
+      layers: ['R', 'U', 'S'],
+    },
+    {
+      index: 120,
+      coords: { x: 2, y: 0, z: 1 },
+      layers: ['R', 'U', 'F'],
+    },
+    {
+      index: 126,
+      coords: { x: 2, y: 1, z: -1 },
+      layers: ['R', 'B', 'E'],
+    },
+    {
+      index: 132,
+      coords: { x: 2, y: 1, z: 0 },
+      layers: ['R', 'E', 'S'],
+    },
+    {
+      index: 138,
+      coords: { x: 2, y: 1, z: 1 },
+      layers: ['R', 'F', 'E'],
+    },
+    {
+      index: 144,
+      coords: { x: 2, y: 2, z: -1 },
+      layers: ['R', 'D', 'B'],
+    },
+    {
+      index: 150,
+      coords: { x: 2, y: 2, z: 0 },
+      layers: ['R', 'D', 'S'],
+    },
+    {
+      index: 156,
+      coords: { x: 2, y: 2, z: 1 },
+      layers: ['R', 'D', 'F'],
+    },
+  ];
+
+  protected permutation: Permutation = new Permutation(27 * 6);
+
+  protected rotation: Rotation = new Rotation()
+    .apply({
+      axis: new Vector3(0, 1, 0),
+      angle: -Math.PI / 4,
+    })
+    .apply({
+      axis: new Vector3(1, 0, 0),
+      angle: -Math.PI / 4,
+    });
+
+  protected stickers: Face[] = Array(27)
+    .fill(['B', 'D', 'F', 'L', 'R', 'U'])
+    .flat();
+
+  constructor() {
+    this.moves
+      .pipe(
+        concatMap((move) => {
+          this.move = move;
+
+          return this.animations.pipe(
+            take(9),
+            toArray(),
+            tap((layers) =>
+              this.permutation.apply(
+                this.permutations[intersection(...layers)[0]]
+              )
+            )
+          );
+        })
+      )
+      .subscribe();
   }
 }
