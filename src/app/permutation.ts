ee7eb279be483be3656835b3b1c39d429c8a22dd
striderhobbit <mariuskoehler@yahoo.de>
2024-02-15 18:12:10 +0100

    toDisjointCycles

--- src/app/permutation.ts
+++ src/app/permutation.ts
@@ -1,7 +1,11 @@
-import { at, times } from 'lodash';
+import { at, pull, times } from 'lodash';
 
 export class Permutation {
   #map: number[];
+
+  get n(): number {
+    return this.#map.length;
+  }
 
   constructor(map: number | number[]) {
     if (typeof map === 'number') {
@@ -37,12 +41,34 @@ export class Permutation {
     return new Permutation(
       this.#map.reduce(
         (inverse, y, x) => Object.assign(inverse, { [y]: x }),
-        Array(this.#map.length)
+        Array(this.n)
       )
     );
   }
 
   preimage(y: number): number {
     return this.#map.indexOf(y);
+  }
+
+  toDisjointCycles(): string {
+    const domain = times(this.n);
+    const cycles: number[][] = [];
+
+    while (domain.length) {
+      let next: number;
+      const cycle: number[] = [(next = domain[0])];
+
+      while (cycle[0] !== (next = this.image(next))) {
+        cycle.push(next);
+      }
+
+      pull(domain, ...cycle);
+
+      if (cycle.length > 1) {
+        cycles.push(cycle);
+      }
+    }
+
+    return cycles.map((cycle) => `(${cycle.join(' ')})`).join('');
   }
 }
